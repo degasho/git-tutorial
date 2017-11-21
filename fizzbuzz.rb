5f98bf4d30532bf0fd5cec5608e26a0a829993e7
j1421 <j1421@matsue-ct.jp>
2017-11-21 10:01:39 +0900

    completion fizzbuzz.rb

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 #fizzbuzzプログラム
-100.times do |i|
-	if ((i+1)%3 == 0) && ((i+1)%5 == 0)
+100.times do |i| #100回ループ
+	if ((i+1)%3 == 0) && ((i+1)%5 == 0) #3か5の倍数
 		p "fizzbuzz"
-	elsif (i+1)%3 == 0
+	elsif (i+1)%3 == 0 #3の倍数のとき
 		p "fizz"
-	elsif (i+1)%5 == 0
+	elsif (i+1)%5 == 0 #5の倍数のとき
 		p "buzz"
 	else
 		p (i+1)
